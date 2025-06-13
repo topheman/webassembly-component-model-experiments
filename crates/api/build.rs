8663686ec2b34ee1b3dbd3a69562ffed827da96d
Christophe Rosset <tophe@topheman.com>
2025-06-13 15:55:29 +0200

    feat: install wasmtime

---
+++ crates/api/build.rs
@@ -0,0 +1,14 @@
+use std::path::PathBuf;
+use wasmtime;
+
+fn main() {
+    let wit_dir = PathBuf::from("../../wit");
+    let gen_dir = PathBuf::from("src/generated");
+    std::fs::create_dir_all(&gen_dir).unwrap();
+
+    wasmtime::component::bindgen!({
+      async: true,
+      path: "../../wit/plugin.wit",
+    });
+
+}
---
+++ crates/api/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod plugin {
+    include!("generated/plugin/mod.rs");
+}
+
+pub mod repl {
+    include!("generated/repl/mod.rs");
+}
