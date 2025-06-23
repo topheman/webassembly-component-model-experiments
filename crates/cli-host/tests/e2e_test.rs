3ed215c78c129ab39c9bf5bbfb80ebe079786fc4
Christophe Rosset <tophe@topheman.com>
2025-06-23 19:51:22 +0200

    tests: update tests

--- crates/cli-host/tests/e2e_test.rs
+++ crates/cli-host/tests/e2e_test.rs
@@ -45,11 +45,8 @@ mod e2e_test {
             .exp_string("[Host] Starting REPL host...")
             .expect("Didn't see startup message");
         session
-            .exp_string("[Host] Loaded plugins config:")
-            .expect("Didn't see plugin configs");
-        session
-            .exp_string("[Host] Loaded env vars:")
-            .expect("Didn't see env vars");
+            .exp_string("[Host] Loading plugin:")
+            .expect("Didn't see plugin loading message");
         session.exp_string("repl>").expect("Didn't see REPL prompt");
     }
 
@@ -68,21 +65,18 @@ mod e2e_test {
             .exp_string("[Host] Starting REPL host...")
             .expect("Didn't see startup message");
         session
-            .exp_string("[Host] Loaded plugins config:")
-            .expect("Didn't see plugin configs");
-        session
-            .exp_string("[Host] Loaded env vars:")
-            .expect("Didn't see env vars");
+            .exp_string("[Host] Loading plugin:")
+            .expect("Didn't see plugin loading message");
         session.exp_string("repl>").expect("Didn't see REPL prompt");
 
         session
             .send_line("greet Tophe")
             .expect("Failed to send command");
         session
-            .exp_string("PluginResponse { status: ReplStatus::Success, stdout: Some(\"Plugin 'greet' executed with payload: Tophe\"), stderr: None }")
-            .expect("Didn't get expected PluginResponse");
+            .exp_string("Hello, Tophe!")
+            .expect("Didn't get expected greeting output");
         session
-            .exp_string("repl>")
+            .exp_string("repl(0)>")
             .expect("Didn't see next REPL prompt");
     }
 
@@ -101,21 +95,48 @@ mod e2e_test {
             .exp_string("[Host] Starting REPL host...")
             .expect("Didn't see startup message");
         session
-            .exp_string("[Host] Loaded plugins config:")
-            .expect("Didn't see plugin configs");
-        session
-            .exp_string("[Host] Loaded env vars:")
-            .expect("Didn't see env vars");
+            .exp_string("[Host] Loading plugin:")
+            .expect("Didn't see plugin loading message");
         session.exp_string("repl>").expect("Didn't see REPL prompt");
 
         session
             .send_line("greet $USER")
             .expect("Failed to send command");
         session
-            .exp_string("PluginResponse { status: ReplStatus::Success, stdout: Some(\"Plugin 'greet' executed with payload: Tophe\"), stderr: None }")
-            .expect("Didn't get expected PluginResponse with variable substitution");
+            .exp_string("Hello, Tophe!")
+            .expect("Didn't get expected greeting output with variable substitution");
         session
-            .exp_string("repl>")
+            .exp_string("repl(0)>")
             .expect("Didn't see next REPL prompt");
+    }
+
+    #[test]
+    fn test_unknown_command() {
+        let project_root = find_project_root();
+        println!("Setting current directory to: {:?}", project_root);
+        std::env::set_current_dir(&project_root).unwrap();
+        let mut session = spawn(
+            "target/debug/cli-host --plugins target/wasm32-wasip1/debug/plugin_greet.wasm",
+            Some(TEST_TIMEOUT),
+        )
+        .expect("Can't launch cli-host with plugin greet");
+
+        session
+            .exp_string("[Host] Starting REPL host...")
+            .expect("Didn't see startup message");
+        session
+            .exp_string("[Host] Loading plugin:")
+            .expect("Didn't see plugin loading message");
+        session.exp_string("repl>").expect("Didn't see REPL prompt");
+
+        session
+            .send_line("nonexistingcmd foo bar")
+            .expect("Failed to send command");
+        session
+            .exp_string("Unknown command: nonexistingcmd. Try `help` to see available commands")
+            .expect("Didn't get expected error output");
+        session
+            .exp_string("repl(1)>")
+            .expect("Didn't see next REPL prompt with $? set to 1");
     }
 }
