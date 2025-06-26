75fd8e92ea93cfcc32415ae6764c6f54853cd96d
Christophe Rosset <tophe@topheman.com>
2025-06-26 17:03:25 +0200

    feat(web-host): add prepareWasmFiles cli

---
+++ packages/web-host/clis/prepareWasmFiles.ts
@@ -0,0 +1,76 @@
+#!/usr/bin/env node --experimental-strip-types --no-warnings
+import { program } from "commander";
+import path from "node:path";
+import fs from "node:fs";
+
+const wasmFiles: Array<{ debug: string; release: string }> = [
+	{
+		debug: "target/wasm32-wasip1/debug/plugin_echo.wasm",
+		release: "target/wasm32-wasip1/release/plugin_echo.wasm",
+	},
+	{
+		debug: "target/wasm32-wasip1/debug/plugin_greet.wasm",
+		release: "target/wasm32-wasip1/release/plugin_greet.wasm",
+	},
+	{
+		debug: "target/wasm32-wasip1/debug/plugin_ls.wasm",
+		release: "target/wasm32-wasip1/release/plugin_ls.wasm",
+	},
+	{
+		debug: "target/wasm32-wasip1/debug/plugin_weather.wasm",
+		release: "target/wasm32-wasip1/release/plugin_weather.wasm",
+	},
+];
+
+function prepareWasmFiles({ mode }: { mode: "debug" | "release" }) {
+	console.log(`Preparing wasm files for mode: ${mode}`);
+	const workspaceRoot = path.join(import.meta.dirname, "..", "..", "..");
+	for (const wasmFile of wasmFiles) {
+		const copyFrom = path.join(workspaceRoot, wasmFile[mode]);
+		const copyTo = path.join(
+			workspaceRoot,
+			"packages",
+			"web-host",
+			"public",
+			path.basename(wasmFile[mode]),
+		);
+		try {
+			fs.copyFileSync(copyFrom, copyTo);
+		} catch (e) {
+			console.log("");
+			console.error(`Failed to copy ${copyFrom} to ${copyTo}`);
+			console.log("");
+			console.error(
+				`Please run the command: just ${mode === "debug" ? "build" : "build-release"}`,
+			);
+			console.log("");
+			process.exit(1);
+		}
+		console.log(`Copied ${copyFrom} to ${copyTo}`);
+	}
+}
+
+const ACCEPTED_MODES = ["debug", "release"];
+
+function assertModeIsValid(mode: string): asserts mode is "debug" | "release" {
+	if (!ACCEPTED_MODES.includes(mode)) {
+		throw new Error(
+			`Error: --mode must be one of: ${ACCEPTED_MODES.join(", ")}.`,
+		);
+	}
+}
+
+function run() {
+	program
+		.description("Prepare wasm files for the web host")
+		.requiredOption("-m, --mode <mode>", "Mode to prepare wasm files for")
+		.action((options) => {
+			const { mode } = options;
+			assertModeIsValid(mode);
+			prepareWasmFiles({ mode });
+		});
+
+	program.parse();
+}
+
+run();
