4de2b73db5b9724dd934280d1170817f6c7e3ccd
Christophe Rosset <tophe@topheman.com>
2025-07-02 16:48:33 +0200

    fix(web-host): fix build

--- packages/web-host/src/wasm/engine.ts
+++ packages/web-host/src/wasm/engine.ts
@@ -1,17 +1,5 @@
 import type { HostApi, PluginApi } from "../types";
 
-const PLUGINS_PATHS = [
-  import.meta.resolve("./generated/plugin_echo/transpiled/plugin_echo.js"),
-  import.meta.resolve(
-    "./generated/plugin_weather/transpiled/plugin_weather.js",
-  ),
-  import.meta.resolve("./generated/plugin_greet/transpiled/plugin_greet.js"),
-  import.meta.resolve("./generated/plugin_ls/transpiled/plugin_ls.js"),
-];
-const REPL_LOGIC_GUEST_PATH = import.meta.resolve(
-  "./generated/repl_logic_guest/transpiled/repl_logic_guest.js",
-);
-
 function makeEngine() {
   const plugins = new Map<string, PluginApi["plugin"]>();
   let replLogicGuest: HostApi | undefined;
@@ -40,26 +28,23 @@ function makeEngine() {
   };
 }
 
-function loadPlugins(paths: string[]): Promise<PluginApi["plugin"][]> {
-  return Promise.all(
-    paths.map(async (path) => {
-      const module = await import(path);
-      console.log("loadPlugins", path, module);
-      return module.plugin;
-    }),
-  );
+function loadPlugins(): Promise<PluginApi["plugin"][]> {
+  return Promise.all([
+    import("./generated/plugin_echo/transpiled/plugin_echo.js"),
+    import("./generated/plugin_weather/transpiled/plugin_weather.js"),
+    import("./generated/plugin_greet/transpiled/plugin_greet.js"),
+    import("./generated/plugin_ls/transpiled/plugin_ls.js"),
+  ]).then((plugins) => plugins.map((plugin) => plugin.plugin));
 }
 
 async function loadReplLogicGuest(): Promise<HostApi> {
-  const module = await import(REPL_LOGIC_GUEST_PATH);
-  console.log("loadReplLogicGuest", module);
-  return module;
+  return import("./generated/repl_logic_guest/transpiled/repl_logic_guest.js");
 }
 
 export async function prepareEngine(): Promise<ReturnType<typeof makeEngine>> {
   const [replLogicGuest, plugins] = await Promise.all([
     loadReplLogicGuest(),
-    loadPlugins(PLUGINS_PATHS),
+    loadPlugins(),
   ]);
   const engine = makeEngine();
   console.log("plugins", plugins);
