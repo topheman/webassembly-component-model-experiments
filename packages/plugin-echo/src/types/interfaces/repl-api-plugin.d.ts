2fed7b171c8f7621a4b05b0bd737e38a42f0175d
Christophe Rosset <tophe@topheman.com>
2025-07-05 23:16:21 +0200

    refactor(plugin-echo/js): remove artifacts

--- packages/plugin-echo/src/types/interfaces/repl-api-plugin.d.ts
+++
@@ -1,5 +0,0 @@
-/** @module Interface repl:api/plugin **/
-export function name(): string;
-export function man(): string;
-export function run(payload: string): PluginResponse;
-export type PluginResponse = import("./repl-api-transport.js").PluginResponse;
