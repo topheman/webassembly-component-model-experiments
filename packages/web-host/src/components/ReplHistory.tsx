f54cf9ebd7558013b2f90908d05a55407401d101
Christophe Rosset <tophe@topheman.com>
2025-07-09 09:40:37 +0200

    feat(web-host): align data-status on the right - doesn't mess with the content

--- packages/web-host/src/components/ReplHistory.tsx
+++ packages/web-host/src/components/ReplHistory.tsx
@@ -18,20 +18,27 @@ export function ReplHistory({
         // biome-ignore lint/suspicious/noArrayIndexKey: no unique key
         <div key={index}>
           {"stdin" in entry && entry.stdin && (
-            <pre className="bg-gray-50 whitespace-pre-wrap">{entry.stdin}</pre>
+            <pre
+              className="bg-gray-50 whitespace-pre-wrap"
+              data-stdtype="stdin"
+            >
+              {entry.stdin}
+            </pre>
           )}
           {"stdout" in entry && entry.stdout && (
             <pre
-              className="bg-green-100 whitespace-pre-wrap before:content-[attr(data-status)]"
-              data-status={entry.status === "success" ? "✅ " : "❌ "}
+              className="bg-green-100 whitespace-pre-wrap before:content-[attr(data-status)] relative before:absolute before:right-0 before:top-0"
+              data-status={entry.status === "success" ? "✅" : "❌"}
+              data-stdtype="stdout"
             >
               {entry.stdout}
             </pre>
           )}
           {"stderr" in entry && entry.stderr && (
             <pre
-              className="bg-red-100 whitespace-pre-wrap before:content-[attr(data-status)]"
-              data-status={entry.status === "success" ? "✅ " : "❌ "}
+              className="bg-red-100 whitespace-pre-wrap before:content-[attr(data-status)] relative before:absolute before:right-0 before:top-0"
+              data-status={entry.status === "success" ? "✅" : "❌"}
+              data-stdtype="stderr"
             >
               {entry.stderr}
             </pre>
