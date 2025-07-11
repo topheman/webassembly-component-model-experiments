5aa74f3a2b9c947b50d6693027a5be7fdfda094e
Christophe Rosset <tophe@topheman.com>
2025-07-11 07:45:33 +0200

    tests(pluginlab): add tests for fail cases when no flag permission is passed

--- crates/pluginlab/tests/e2e_test.rs
+++ crates/pluginlab/tests/e2e_test.rs
@@ -339,4 +339,61 @@ mod e2e_test {
             .exp_string("foo bar yolo")
             .expect("Didn't get expected echo output for $0 - should be the same as the last non failingcommand");
     }
+
+    #[test]
+    fn test_without_permission_allow_network() {
+        let project_root = find_project_root();
+        println!("Setting current directory to: {:?}", project_root);
+        std::env::set_current_dir(&project_root).unwrap();
+        let mut session = spawn(
+            &build_command(&["plugin_weather.wasm"], "repl_logic_guest.wasm"),
+            Some(TEST_TIMEOUT),
+        )
+        .expect("Can't launch pluginlab with plugin greet");
+
+        session
+            .exp_string("[Host] Starting REPL host...")
+            .expect("Didn't see startup message");
+        session
+            .exp_string("[Host] Loading plugin:")
+            .expect("Didn't see plugin loading message");
+        session
+            .exp_string("repl(0)>")
+            .expect("Didn't see REPL prompt");
+        session
+            .send_line("weather Paris")
+            .expect("Failed to send command");
+        session
+            .exp_string("Error fetching weather: PermissionDenied: network access to wttr.in is not allowed")
+            .expect("Didn't get expected error output");
+    }
+
+    #[test]
+    fn test_without_permission_allow_read() {
+        let project_root = find_project_root();
+        println!("Setting current directory to: {:?}", project_root);
+        std::env::set_current_dir(&project_root).unwrap();
+        let mut session = spawn(
+            &format!(
+                "{} --dir tmp/filesystem",
+                &build_command(&["plugin_ls.wasm"], "repl_logic_guest.wasm")
+            ),
+            Some(TEST_TIMEOUT),
+        )
+        .expect("Can't launch pluginlab with plugin greet");
+
+        session
+            .exp_string("[Host] Starting REPL host...")
+            .expect("Didn't see startup message");
+        session
+            .exp_string("[Host] Loading plugin:")
+            .expect("Didn't see plugin loading message");
+        session
+            .exp_string("repl(0)>")
+            .expect("Didn't see REPL prompt");
+        session.send_line("ls").expect("Failed to send command");
+        session
+            .exp_string("ls: : Operation not permitted")
+            .expect("Didn't get expected error output");
+    }
 }
