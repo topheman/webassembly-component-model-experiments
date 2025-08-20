d21b6c430a33b8491d2e5a937d8eba3b583dc05e
Christophe Rosset <tophe@topheman.com>
2025-08-20 10:31:39 +0200

    fix(e2e): fillAndSubmitCommand now checks the stdout and stderr after the stdin if passed expectStdout or expectStderr
    Avoids false positives

--- packages/web-host/tests/utils.ts
+++ packages/web-host/tests/utils.ts
@@ -1,4 +1,6 @@
-import { expect, type Page } from "@playwright/test";
+import { expect, type Locator, type Page } from "@playwright/test";
+
+const NEXT_FRAME_DELAY = 64;
 
 /**
  * Get the last std output of the given type
@@ -8,6 +10,26 @@ export async function getLastStd(
   type: "stdin" | "stdout" | "stderr",
 ) {
   return await page.locator(`[data-stdtype='${type}']`).last();
+}
+
+/**
+ * Get the last std output of the given type after the given locator
+ * This is useful to get the last std output after a command has been submitted
+ * This ensures you don't have false positives when checking the last std output
+ */
+export async function getLastStdAfter(
+  page: Page,
+  type: "stdin" | "stdout" | "stderr",
+  stdLocator: Locator,
+) {
+  const stdinIndex = await stdLocator.getAttribute("data-std-index");
+  return await page
+    .locator(`[data-std-index='${stdinIndex}'] ~ [data-stdtype='${type}']`)
+    .last();
+}
+
+async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 /**
@@ -30,14 +52,15 @@ export async function fillAndSubmitCommand(
   const input = await page.getByPlaceholder("Type a command...");
   await input.fill(command);
   await input.press("Enter");
+  await sleep(NEXT_FRAME_DELAY);
   const stdin = await getLastStd(page, "stdin");
   await expect(stdin).toHaveText(expectStdin);
   if (expectStdout) {
-    const stdout = await getLastStd(page, "stdout");
+    const stdout = await getLastStdAfter(page, "stdout", stdin);
     await expect(stdout).toHaveText(expectStdout);
   }
   if (expectStderr) {
-    const stderr = await getLastStd(page, "stderr");
+    const stderr = await getLastStdAfter(page, "stderr", stdin);
     await expect(stderr).toHaveText(expectStderr);
   }
 }
